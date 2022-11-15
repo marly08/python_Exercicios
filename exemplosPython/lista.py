d889347e51ddd606c4b0637a4ea28ec81763c9aa
Marly Aparecida <magmarly08@gmail.com>
2022-11-15 09:36:06 -0300

    atualização

--- exemplosPython/lista.py
+++ exemplosPython/lista.py
@@ -11,13 +11,13 @@ print("indice ::-1 ", nome[::-1])
 for indice in nome:
     print(indice)
 
-lista_numeros = [2,5,45,3,7,8,90]
+lista_numeros = [2, 5, 45, 3, 7, 8, 90]
 for indice in lista_numeros:
     print(indice)
 
-print( " Numero de elemntos da lista: " + str(len(lista_numeros)))
+print(" Numero de elemntos da lista: " + str(len(lista_numeros)))
 lista_numeros.append(100)
-lista_numeros.insert(0,0)
+lista_numeros.insert(0, 0)
 lista_numeros.sort()
 print("lista ordenada com SORT() ", lista_numeros)
 lista_numeros.pop(2)
--- exemplosPython/lista3.py
+++ exemplosPython/lista3.py
@@ -1,20 +1,19 @@
 from ctypes.wintypes import PINT
 
 
-lista_lojas = ["Pirapora", "Buritizeiro", "Varzea da Palma" ]
+lista_lojas = ["Pirapora", "Buritizeiro", "Varzea da Palma"]
 lista_faturamento = [200, 600, 350]
 
 
 #remover= del_remove_pop
-#vincular listas
-resultados =[]
+# vincular listas
+resultados = []
 for i in range(3):
     tupla = (lista_lojas[i-1], lista_faturamento[i-1])
     resultados.append(tupla)
-    
+
 print(resultados)
 print("faturamento loja Pirapora: ", resultados[1][1])
-
 
 
 profissao = ["medico", "professor", "cozinheira", "ator", "ator"]
